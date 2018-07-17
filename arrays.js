bad2e4b049761702824cdae432e539ca77c51c6c
Travis Avey <avey.travis@gmail.com>
2018-07-17 22:58:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+];
+
+function addElementToBeginningOfArray(array, ele) {
+  array.unshift(ele);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, ele) {
+  
+}
