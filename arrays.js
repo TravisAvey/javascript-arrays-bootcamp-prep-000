2d56856472c753ee6f2ab1f067b33426128288dc
Travis Avey <avey.travis@gmail.com>
2018-07-17 22:59:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,12 @@ var chocolateBars = [
 ];
 
 function addElementToBeginningOfArray(array, ele) {
+  var arr = [ele, ...array];
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, ele) {
   array.unshift(ele);
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, ele) {
-  
-}
